4a880a68f7d53a2755ce1f213ff233a8f0dcacfd
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-08 14:55:17 -0300

    Design do gráfico de anscombe alterado

--- R/anscombe.R
+++ R/anscombe.R
@@ -6,17 +6,29 @@ library(ggplot2)
 
 ggplot(anscombe, aes(x = x1, y = y1)) +
   geom_point() +
-  geom_smooth(method = "lm", color = "Red", se = FALSE)
+  geom_smooth(method = "lm", color = "Red", se = FALSE) +
+  ggtitle("Gráfico x1 vs y1") +
+  theme_test() +
+  theme(plot.title = element_text(hjust=0.5))
 
 ggplot(anscombe, aes(x = x2, y = y2)) +
   geom_point() +
-  geom_smooth(method = "lm", color = "Red", se = FALSE)
+  geom_smooth(method = "lm", color = "Red", se = FALSE) +
+  ggtitle("Gráfico x2 vs y2") +
+  theme_test() +
+  theme(plot.title = element_text(hjust=0.5))
 
 ggplot(anscombe, aes(x = x3, y = y3)) +
   geom_point() +
-  geom_smooth(method = "lm", color = "Red", se = FALSE)
+  geom_smooth(method = "lm", color = "Red", se = FALSE) +
+  ggtitle("Gráfico x3 vs y3") +
+  theme_test() +
+  theme(plot.title = element_text(hjust=0.5))
 
 ggplot(anscombe, aes(x = x4, y = y4)) +
   geom_point() +
-  geom_smooth(method = "lm", color = "Red", se = FALSE)
+  geom_smooth(method = "lm", color = "Red", se = FALSE) +
+  ggtitle("Gráfico x4 vs y4") +
+  theme_test() +
+  theme(plot.title = element_text(hjust=0.5))
 
