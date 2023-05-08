a5358c5cc2651f584cca57c9caa16892539fb2d7
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-08 17:07:22 -0300

    Tirei a fonte

--- R/historical_visualization.R
+++ R/historical_visualization.R
@@ -26,16 +26,16 @@ ggplot(molten_data, aes(x = year, y = value)) +
   scale_color_manual(values = c("darkred", "gold3"), guide = "none") + 
   scale_fill_manual(values = c("#90752d", "#BB5766"), guide = "none") + 
   theme_bw() + 
-  annotate("text", x = year[5],        y = 100000, label = "Line", angle = 25, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[6] - 100,  y = 104000, label = "of",  angle = 0, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[7],        y = 101000, label = "Imports", angle = 340, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[5] + 400,  y = 73000,  label = "Line",  angle = 345, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[6],        y = 70000,  label = "of",  angle = 330, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[7] - 200,  y = 64000,  label = "Exports",  angle = 335, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[8],        y = 83000,  label = "italic('BALANCE AGAINST')",  angle = 0, family = "Times New Roman", parse = TRUE) +
-  annotate("text", x = year[16] + 400, y = 110000, label = "italic('BALANCE in\nFAVOUR of\nENGLAND')",  angle = 0, family = "Times New Roman", parse = TRUE) +
-  annotate("text", x = year[16],       y = 82000,  label = "Imports",  angle = 30, size = 3, family = "Times New Roman") +
-  annotate("text", x = year[14] + 200, y = 131000, label = "Exports",  angle = 65, size = 3, family = "Times New Roman") +
+  annotate("text", x = year[5],        y = 100000, label = "Line", angle = 25, size = 3) +
+  annotate("text", x = year[6] - 100,  y = 104000, label = "of",  angle = 0, size = 3) +
+  annotate("text", x = year[7],        y = 101000, label = "Imports", angle = 340, size = 3) +
+  annotate("text", x = year[5] + 400,  y = 73000,  label = "Line",  angle = 345, size = 3) +
+  annotate("text", x = year[6],        y = 70000,  label = "of",  angle = 330, size = 3) +
+  annotate("text", x = year[7] - 200,  y = 64000,  label = "Exports",  angle = 335, size = 3) +
+  annotate("text", x = year[8],        y = 83000,  label = "italic('BALANCE AGAINST')",  angle = 0, parse = TRUE) +
+  annotate("text", x = year[16] + 400, y = 110000, label = "italic('BALANCE in\nFAVOUR of\nENGLAND')",  angle = 0, parse = TRUE) +
+  annotate("text", x = year[16],       y = 82000,  label = "Imports",  angle = 30, size = 3) +
+  annotate("text", x = year[14] + 200, y = 131000, label = "Exports",  angle = 65, size = 3) +
   ggtitle("Exports and Imports to and from DENMARK & NORWAY from 1700 to 1780") + 
   scale_x_date(breaks = seq(year[1], year[18], by = "10 years"), 
                labels = format(seq(year[1], year[18], by = "10 years"), "%Y")) + 
@@ -43,10 +43,10 @@ ggplot(molten_data, aes(x = year, y = value)) +
                      labels = seq(0, 190, 10),
                      position = "right",
                      limits = c(10000, NA)) +
-  theme(title = element_text(size = 8, face = 'bold', family = "Times New Roman"),
+  theme(title = element_text(size = 8, face = 'bold'),
         plot.title = element_text(hjust=0.5),
         axis.title = element_blank(),
-        axis.text = element_text(family = "Times New Roman"), 
+        axis.text = element_text(), 
         panel.grid.minor = element_blank(),
         panel.grid.major = element_line(color = "#c8c8c6"),
         panel.background = element_rect(fill = "#fcfcfa"))
