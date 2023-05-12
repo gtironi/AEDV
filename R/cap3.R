4749f69f3d3e3d2b87410fd5496edf4828c05c19
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-12 09:14:29 -0300

    Fiz mais do exercico b

--- R/cap3.R
+++ R/cap3.R
@@ -51,10 +51,42 @@ p8 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp, color =
 
 p8
 
-p9 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp, color = "purple")) + 
-  geom_point() +
+p9 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(color = "purple") +
   geom_smooth(method =  "loess") + 
   scale_x_log10()
 
 p9
 
+p10 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(alpha = 0.3) +
+  geom_smooth(color = "orange", se = FALSE, size = 8, method =  "lm") + 
+  scale_x_log10()
+
+p10
+
+
+p11 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(alpha = 0.3) +
+  geom_smooth(method =  "lm") + 
+  scale_x_log10(labels = scales::dollar) +
+  labs(x = "GDP Per capita", y = "Life Expectancy in Years",
+       title = "Economic Growth and Life Expectancy",
+       subtitle = "Data points are country-years",
+       caption = "Source: Gapminder.")
+
+p11
+
+p12 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp, color = continent, fill = continent)) + 
+  geom_point() +
+  geom_smooth(method =  "loess") + 
+  scale_x_log10()
+
+p12
+
+p13 <- ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point() +
+  geom_smooth(method =  "loess") + 
+  scale_x_log10()
+
+p13
